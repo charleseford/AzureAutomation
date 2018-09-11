42196bbae16e3a90c85b78bb6f2bdd0d5ec12c17
charleseford <charleseford@yahoo.com>
2018-09-11 14:44:13 -0500

    Created Runbook2.ps1 in AzureAutomation.

---
+++ Runnbook/Runbook2.ps1
@@ -0,0 +1,17 @@
+param(
+[Parameter (Mandatory=$True)]
+[String]$testparam
+)
+Write-output "Starting..."
+$Conn = Get-AutomationConnection -Name AzureRunAsConnection
+$conn | fl *
+Add-AzureRMAccount -ServicePrincipal -Tenant $conn.tenantid -applicationid $conn.applicationid -CertificateThumbprint $conn.CertificateThumbprint
+#newer cmdlet
+#Connect-AzureRmAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+$resourcegroups = Get-AzureRmResourcegroup
+foreach ($resourcegroup in $resourcegroups)
+{
+    Write-Output $resourcegroup.resourcegroupname
+    $resourcegroup | Set-AzureRmResourceGroup -Tag @{Name="TestTag";Value="$testparam-$(get-date)"}
+
+}
